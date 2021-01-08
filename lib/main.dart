c0141a300b9bdeb016bf6d46c5e934500f1f426d
Hendrick <sebastinesoacatp@gmail.com>
2021-01-08 22:17:32 +0100

    Making the app interactive

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
-
   // Title Section Widget
   Widget titleSection = Container(
     padding: const EdgeInsets.all(32),
@@ -38,11 +37,7 @@ class MyApp extends StatelessWidget {
             ),
         ),
         /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
+        FavouriteWidget(),
       ],
     ),
   );
@@ -119,5 +114,53 @@ class MyApp extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class FavouriteWidget extends StatefulWidget{
+  @override
+  _FavouriteWidgetState createState() => _FavouriteWidgetState();
+}
+
+class _FavouriteWidgetState extends State<FavouriteWidget> {
+  bool _isFavourited = true;
+  int _favouriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavourited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavourite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favouriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFavourite() {
+    setState(() {
+      if(_isFavourited){
+        _favouriteCount -= 1;
+        _isFavourited = false;
+      } else{
+        _favouriteCount += 1;
+        _isFavourited = true;
+      }
+    });
   }
 }
