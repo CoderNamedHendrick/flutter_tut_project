8f62ef371dc618965a816c7a3bd7346b3b56634b
Hendrick <sebastinesoacatp@gmail.com>
2021-01-10 17:08:00 +0100

    Using Named routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,56 +2,66 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+        title: 'Name Routes Demo',
+        // Start the app with the "/" named route. In this case, the app starts
+        // on the FirstScreen widget.
+        initialRoute: '/',
+        routes: {
+          // When navigating to the "/" route, build the FirstScreen widget.
+          '/': (context) => FirstScreen(),
+          // When navigating to the "/second" route, build the SecondScreen widget.
+          '/second': (context) => SecondScreen(),
+        },
+      ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MainScreen(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return
+//   }
+// }
 
-class MainScreen extends StatelessWidget {
+class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Screen'),
+        title: Text(
+            'Second Route'),
       ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (_) {
-            return DetailScreen();
-          }));
-        },
-        child: Hero(
-          tag: 'imageHero',
-          child: Image.asset('images/mypic.png'),
+      body: Center(
+        child: ElevatedButton(
+          onPressed:() {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
         ),
       ),
     );
   }
 }
 
-class DetailScreen extends StatelessWidget {
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-        body: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open route'),
+          onPressed: (){
+            Navigator.pushNamed(context, '/second');
+            // Navigate to second route when tapped.
           },
-          child: Center(
-            child: Hero(
-              tag: 'imageHero',
-              child: Image.asset('images/mypic.png')
-            ),
-          ),
         ),
+      ),
     );
   }
 }
