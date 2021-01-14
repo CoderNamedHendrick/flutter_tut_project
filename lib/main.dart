db24faf0a7ae04969608e2132e501ad6840f235d
Hendrick <sebastinesoacatp@gmail.com>
2021-01-14 12:56:11 +0100

    Sending result across screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Returning Data',
+      home: HomeScreen(),
     );
   }
 }
@@ -26,5 +29,69 @@ class HomeScreen extends StatelessWidget{
 
 }
 
-class SelectionButton {
+class SelectionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          _navigateAndDisplaySelection(context);
+        },
+        child: Text("Pick an option, any option!")
+    );
+  }
+
+  // A method that launches the SelectionScreen and awaits the
+  // result from Navigator.pop
+  _navigateAndDisplaySelection(BuildContext context) async {
+    // Navigator.push returns a Future that completes after calling
+    // Navigator.pop on the Selection Screen.
+    final result = await Navigator.push(
+      context,
+      // Create the SelectionScreen in the next step
+      MaterialPageRoute(builder: (context) => SelectionScreen()),
+    );
+
+    // After the Selection Screen returns a result, hide any previous snackbars
+    // and show the new result
+    Scaffold.of(context)
+    ..removeCurrentSnackBar()
+    ..showSnackBar(SnackBar(content: Text("$result")));
+  }
+}
+
+class SelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Pop here with "Yep"
+                  Navigator.pop(context, 'Yep!');
+                },
+                child: Text('Yep!'),
+              ),),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Pop here with "Nope"
+                  Navigator.pop(context, 'Nope');
+                },
+                child: Text('Nope.'),
+              ),)
+          ],
+        ),
+      ),
+    );
+  }
+
 }
