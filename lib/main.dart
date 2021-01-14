1566a6a9ca754a4a7f5455a0846f7ad9dae20408
Hendrick <sebastinesoacatp@gmail.com>
2021-01-14 20:44:58 +0100

    Sending info to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class Todo {
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
+}
+
 void main() {
   runApp(MyApp());
 }
@@ -10,88 +17,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Returning Data',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Returning Data Demo"),
-      ),
-      body: Center(child: SelectionButton(),),
-    );
-  }
-
-}
-
-class SelectionButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: () {
-          _navigateAndDisplaySelection(context);
-        },
-        child: Text("Pick an option, any option!")
-    );
-  }
-
-  // A method that launches the SelectionScreen and awaits the
-  // result from Navigator.pop
-  _navigateAndDisplaySelection(BuildContext context) async {
-    // Navigator.push returns a Future that completes after calling
-    // Navigator.pop on the Selection Screen.
-    final result = await Navigator.push(
-      context,
-      // Create the SelectionScreen in the next step
-      MaterialPageRoute(builder: (context) => SelectionScreen()),
-    );
-
-    // After the Selection Screen returns a result, hide any previous snackbars
-    // and show the new result
-    Scaffold.of(context)
-    ..removeCurrentSnackBar()
-    ..showSnackBar(SnackBar(content: Text("$result")));
-  }
-}
-
-class SelectionScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pick an option'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Pop here with "Yep"
-                  Navigator.pop(context, 'Yep!');
-                },
-                child: Text('Yep!'),
-              ),),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Pop here with "Nope"
-                  Navigator.pop(context, 'Nope');
-                },
-                child: Text('Nope.'),
-              ),)
-          ],
+      title: 'Passing Data',
+      home: TodoScreen(
+        todos: List.generate(
+            20,
+                (index) => Todo(
+                    'Todo $index',
+                    'A description of what needs to be done for Todo $index'
+                ),
         ),
       ),
     );
   }
+}
 
+class TodoScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  // requiring the list of todos
+  TodoScreen({Key key, @required this.todos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(todos[index].title),
+            onTap: () {
+              // When a user taps the ListTile, navigate to the DetailScreen
+              // We aren't only creating a DetailScreen, we are also
+              // passing the current todo through to it
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(),
+                  // Pass the arguments as part of the RouteSettings. The
+                  // DetailScreen reads the arguments from these settings.
+                  settings: RouteSettings(
+                    arguments: todos[index],
+                  )
+                )
+              );
+            },
+          );
+        }
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Todo todo = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Text(todo.description),
+      ),
+    );
+  }
 }
