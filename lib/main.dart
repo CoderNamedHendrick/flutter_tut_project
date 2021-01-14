5ea5fabc7ccf782b155414cdf90af4c0d5dd3d9d
Hendrick <sebastinesoacatp@gmail.com>
2021-01-14 12:30:51 +0100

    Sending data across screens tutorial

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Returning Data Demo"),
+      ),
+      body: Center(child: SelectionButton(),),
+    );
+  }
+
+}
+
+class SelectionButton {
+}
